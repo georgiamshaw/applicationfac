ed84df4a3af3ad20db0767a58247af226d674383
Georgia Shaw <georgia@cranbrookw4.com>
2019-03-27 12:57:46 +0000

    adding js and fixing structure
    -add js typewriter effect to h1 and h2
    -fixing structure of html
    -adding id instead of class and changing in js and css

---
+++ scripts/typeWriter.js
@@ -0,0 +1,27 @@
+//Typewriter effect on h1
+var i = 0;
+var typeWriterH1 = "Georgia Shaw.";
+var j = 0;
+var typeWriterH2 = "Founders and Coders Application.";
+var k = 0;
+var typeWriterH22 = "April 2019.";
+var speed = 90;
+var speed1 = 70;
+
+function typeWriter() {
+  if (i < typeWriterH1.length) {
+    document.getElementById("typeWriterHeading1").innerHTML += typeWriterH1.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+  else if (i == typeWriterH1.length && j < typeWriterH2.length) {
+    document.getElementById("typeWriterHeading2").innerHTML += typeWriterH2.charAt(j);
+    j++;
+    setTimeout(typeWriter, speed1);
+  }
+  else if (j == typeWriterH2.length && k < typeWriterH22.length) {
+    document.getElementById("typeWriterHeading22").innerHTML += typeWriterH22.charAt(k);
+    k++;
+    setTimeout(typeWriter, speed1);
+  }
+} typeWriter();
