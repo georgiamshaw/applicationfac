beae6915651f8c1e9f0c6768cf27cdcf113ff6f0
Georgia Shaw <georgia@cranbrookw4.com>
2019-04-16 14:43:40 +0100

    js burger changes
    -removed burger.js and moved js to drop-down-menu.js
    -removed onclick functions from html
    -made js smoother and easier to function

--- scripts/drop-down-menu.js
+++ scripts/drop-down-menu.js
@@ -1,18 +1,33 @@
 // Click on the button to toggle between hiding and showing content
-function dropDownFunction() {
-  document.getElementById("dropDownMenu").classList.toggle("appear");
-}
+const menuButton = document.getElementById("menu-button");
+const burger = document.getElementById("burger-container");
+const dropDown = document.getElementById("dropDownMenu");
+const dropdowns = document.getElementsByClassName("dropdown-content");
 
-// Close the dropdown menu if someone clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches(".menu-button")) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("appear")) {
-        openDropdown.classList.remove("appear");
-      }
-    }
-  }
-}
+
+menuButton.addEventListener("click", function(e) {
+    menuButton.classList.toggle("change");
+    dropDown.classList.toggle("appear");
+    console.log('clicked');
+}, false);
+
+burger.addEventListener("click", function(e) {
+    burger.classList.toggle("change");
+    dropDown.classList.toggle("appear");
+    console.log('clicked');
+}, false);
+
+//
+// // Close the dropdown menu if someone clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches("#dropdownMenu")) {
+//     console.log("close-menu");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains("appear")) {
+//         openDropdown.classList.remove("appear");
+//       }
+//     }
+//   }
+// }
